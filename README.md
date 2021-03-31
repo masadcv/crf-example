d1b75efd993d42b4419447c28a9c42f05c9ad270
masadcv <3410225+masadcv@users.noreply.github.com>
2021-03-31 22:57:47 +0100

    updating the example/readme

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-**To Run**
+## Simple CRF example forked from: https://github.com/charliebudd/crf-example
+Here is the list of changes/updates made to original repo:
+- Added support for running example on CPU only
+- Added description of steps to setup and run example
+- Restructuring and timing decorator for both CPU/GPU versions added
+- Added screenshot in readme, showing the expected output
 
-before running "crf_test.py"...
+The following has been run on Ubuntu 20.04 with Python 3.8
 
-ensure the environment variable BUILD_MONAI=1 is set... "export BUILD_MONAI=1" (on linux)
+## Prepare venv
+- [Optional] Create a virtualenv 
 
-install the requirements... "pip install -r requirements.txt"
+    `virtualenv venv -p python3.8`
 
-BUILD_MONAI should be set before installing monai
+- Activate venv
+    
+    `source venv/bin/activate`
+
+- [Optional] If compiling for CPU, install torch for CPU
+
+    `pip install torch==1.8.0+cpu torchvision==0.9.0^Cpu torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html`
+
+- Install pre-requisites (build monai)
+     
+     `BUILD_MONAI=1 pip install -r requirements.txt`
+
+
+## Run example
+
+`python crf_test.py`
+
+If all goes well, you will see the following output:
+
+![2D test output](figures/output.png)
+
+Change `run_in_3d = False` to `run_in_3d = True` to run the example in 3D
+
+
+
--- crf_test.py
+++ crf_test.py
@@ -1,68 +1,87 @@
+from functools import wraps
+from time import time
+
 import matplotlib.pyplot as plt
 import nibabel
 import torch
 
 from monai.networks.blocks import CRF
-from monai.utils.profiling import torch_profiler_time_end_to_end
 
-@torch_profiler_time_end_to_end
-def run_timed(func, args):
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        tic = time()
+        result = f(*args, **kw)
+        toc = time()
+        print('func:%r took: %2.4f sec' % \
+          (f.__name__, toc-tic))
+        return result
+    return wrap
+
+@timing
+def run_crf_timed(func, args):
     return func(*args)
 
-# Run parameters
-run_in_3d = False
-slice_index = 72
+if __name__ == '__main__':
 
-# Loading data from files
-ct = nibabel.load("abdominal_ct.nii").get_fdata()
-logits = nibabel.load("abdominal_logits.nii").get_fdata()
+    # allow for running on CPU when GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-# Preparing data
-if not run_in_3d:
-    ct = ct[..., slice_index]
-    logits = logits[..., slice_index]
+    # Run parameters
+    run_in_3d = True
+    slice_index = 72
 
-ct_tensor = torch.from_numpy(ct).type(torch.FloatTensor).cuda()
-logits_tensor = torch.from_numpy(logits).type(torch.FloatTensor).cuda()
+    # Loading data from files
+    ct = nibabel.load("abdominal_ct.nii").get_fdata()
+    logits = nibabel.load("abdominal_logits.nii").get_fdata()
 
-# Adding noise
-logits_tensor *= (torch.rand_like(logits_tensor) + 1) * 0.5
+    # Preparing data
+    if not run_in_3d:
+        ct = ct[..., slice_index]
+        logits = logits[..., slice_index]
 
-# CRF paramters
-crf = CRF(
-    iterations = 5, 
-    bilateral_weight = 3.0,
-    gaussian_weight = 1.0,
-    bilateral_spatial_sigma = 5.0,
-    bilateral_color_sigma = 0.5,
-    gaussian_spatial_sigma = 5.0,
-    compatability_kernel_range = 1,
-)
+    ct_tensor = torch.from_numpy(ct).type(torch.FloatTensor).to(device)
+    logits_tensor = torch.from_numpy(logits).type(torch.FloatTensor).to(device)
 
-# Run CRF and take labelsfrom input and output tensors
-logits_smoothed_tensor = run_timed(crf, (logits_tensor, ct_tensor))
-labels_tensor = torch.argmax(logits_tensor, dim=1, keepdim=True)
-labels_smoothed_tensor = torch.argmax(logits_smoothed_tensor, dim=1, keepdim=True)
+    # Adding noise
+    logits_tensor *= (torch.rand_like(logits_tensor) + 1) * 0.5
 
-# Reading back data
-ct = ct_tensor.squeeze(0).movedim(0, -1).cpu()
-labels = labels_tensor.squeeze(0).movedim(0, -1).cpu()
-labels_smoothed = labels_smoothed_tensor.squeeze(0).movedim(0, -1).cpu()
+    # CRF paramters
+    crf = CRF(
+        iterations = 5, 
+        bilateral_weight = 3.0,
+        gaussian_weight = 1.0,
+        bilateral_spatial_sigma = 5.0,
+        bilateral_color_sigma = 0.5,
+        gaussian_spatial_sigma = 5.0,
+        compatability_kernel_range = 1,
+    )
 
-# Slicing for display if needed
-if run_in_3d:
-    ct = ct[..., slice_index]
-    labels = labels[..., slice_index]
-    labels_smoothed = labels_smoothed[..., slice_index]
+    # Run CRF and take labelsfrom input and output tensors
+    logits_smoothed_tensor = run_crf_timed(crf, (logits_tensor, ct_tensor))
 
-# Display slices
-plt.subplot(131).axis("off")
-plt.title("input image")
-plt.imshow(ct)
-plt.subplot(132).axis("off")
-plt.title("input labels")
-plt.imshow(labels)
-plt.subplot(133).axis("off")
-plt.title("output labels")
-plt.imshow(labels_smoothed)
-plt.show()
+    labels_tensor = torch.argmax(logits_tensor, dim=1, keepdim=True)
+    labels_smoothed_tensor = torch.argmax(logits_smoothed_tensor, dim=1, keepdim=True)
+
+    # Reading back data
+    ct = torch.squeeze(ct_tensor).cpu()
+    labels = torch.squeeze(labels_tensor).cpu()
+    labels_smoothed = torch.squeeze(labels_smoothed_tensor).cpu()
+
+    # Slicing for display if needed
+    if run_in_3d:
+        ct = ct[..., slice_index]
+        labels = labels[..., slice_index]
+        labels_smoothed = labels_smoothed[..., slice_index]
+
+    # Display slices
+    plt.subplot(131).axis("off")
+    plt.title("input image")
+    plt.imshow(ct)
+    plt.subplot(132).axis("off")
+    plt.title("input labels")
+    plt.imshow(labels)
+    plt.subplot(133).axis("off")
+    plt.title("output labels")
+    plt.imshow(labels_smoothed)
+    plt.show()
